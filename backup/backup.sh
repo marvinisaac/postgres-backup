84b2b5209ed5bfdf90f92f3c63d4faafccc25f17
Marvin Isaac <4396841+marvinisaac@users.noreply.github.com>
2024-11-02 16:37:04 +0000

    :wrench: Create separate directory for backup data

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-rm *.sql
-rm *.sql.bz2
-rm *.sql.gz
+rm /backup/data/*.sql
+rm /backup/data/*.sql.bz2
+rm /backup/data/*.sql.gz
 
-FILE=`date +%Y-%m-%d-%H-%M-%S`.sql
+FILE=/backup/data/`date +%Y-%m-%d-%H-%M-%S`.sql
 pg_dumpall --clean --user postgres | gzip -9 > $FILE.gz
 # pg_dumpall --clean --user postgres | bzip2 -9 > $FILE.bz2
